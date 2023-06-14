1375621269f052b8ca5a84e064906884b283effa
evgeniyvelboienko <evgeniyvelboienko@o-s-i.org>
2023-06-14 16:44:20 +0300

    upd

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        <input type="checkbox">
+        <p>{{ todoItem.title }}</p>
+    </div>
+</template>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+
+<template>
+    <div>
+        <h1>My Todo List</h1>
+        <p>{{ todoItem.title }}</p>
+    </div>
+</template>
